6bb50c404eb0cf8374cf14cf4e194178a229ff8d
Olis Acosta <infobyolisjo@gmail.com>
2025-08-16 14:58:18 -0400

    fix(build): next.config limpio + handler raíz

--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-<<<<<<< HEAD
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['*']
-    }
-  }
-}
-module.exports = nextConfig
-=======
-  typescript: { ignoreBuildErrors: true } // ← temporal para poder desplegar
-};
+const nextConfig = { reactStrictMode: true };
 module.exports = nextConfig;
-
->>>>>>> 4519612185e08ac69cc8235c6adeafe63cbd010e
